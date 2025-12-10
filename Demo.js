cb259b60aaaa6158f0be6360dced4d5f05ba13c8
YAKANKSHAKESAVAPATNAM <yakankshakesavapatnam@gmail.com>
2025-12-10 21:38:00 +0530

    import export in node

---
+++ Demo.js
@@ -0,0 +1,9 @@
+const registeruser=require('./Registration.js');
+function sayHello(){
+    console.log("Hello, from Node JS");
+}
+sayHello();
+registeruser.checkforuseer()
+registeruser.registeruser()
+
+
---
+++ Registration.js
@@ -0,0 +1,10 @@
+function checkforuseer(){
+    console.log("checking for user");
+}
+function registeruser(){
+    console.log("User registered successfully");
+}
+module.exports={
+    checkforuseer:checkforuseer,
+    registeruser:registeruser
+}
